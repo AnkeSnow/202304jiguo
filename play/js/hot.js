3fd63a8d362267bb0172babd4976980fa01a1e74
zyh <1428689567@qq.com>
2023-09-22 20:27:25 +0800

    guid完成

---
+++ play/js/hot.js
@@ -0,0 +1,43 @@
+
+//试用页面渲染数据
+var ul = document.querySelector('#myDiv')
+var ajax_ = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
+ajax_.open("get", "http://127.0.0.1:3000/play/hot");
+ajax_.send();
+ajax_.onreadystatechange = function () {
+    if (ajax_.readyState == 4) {
+        if (ajax_.status == 200) {
+            var data = JSON.parse(ajax_.responseText);
+            var str = '';
+            for (var item of data) {
+                console.log(item);
+                // console.log(data);
+                for (var i = 0; i < item.length; i++) {
+                    console.log(item[i].img);
+                    console.log(item[i].text);
+                    str += `<li>
+                <a href="use/detail.html">
+                    <img src="${item[i].img}" width="220" height="130" />
+                    <div class="info">
+                        <p class="name">${item[i].description}<span>${item[i].text}</span></p>
+                        <div class="tip fix">
+                            <span class="price left">${item[i].price}</span>
+                            <div class="right icon">
+                                <span class="xin">${item[i].like}</span>
+                                <span class="look">${item[i].words}</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </li>`
+
+                }
+            }
+
+
+        }
+        ul.innerHTML = str;
+
+
+    }
+}
